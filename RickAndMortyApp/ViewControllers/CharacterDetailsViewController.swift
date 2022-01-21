b539d1d61e539f27b87fbce7fdb44b84f92c56aa
Alexey Kulish <alexkulish1993@gmail.com>
2022-01-21 14:52:21 +0300

    Setup CharacterTableViewVC, CharacterDetailsVC, CustomTableViewCell + work on storyboard

---
+++ RickAndMortyApp/ViewControllers/CharacterDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CharacterDetailsViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Alex Kulish on 21.01.2022.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
